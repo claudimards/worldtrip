8ce3ec6d63536fbcd1927a7638be0a9272cb42f4
Claudimar Bezerra <claudimards@gmail.com>
2022-02-09 20:32:25 -0300

    Initial project structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>WorldTrip</title>
+      </Head>
+
+      <h1>Hello to WorldTrip, my first nextjs app from total zero!</h1>
+    </>
+  )
+}
